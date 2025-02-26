a583c006a59c4e7cba9f0205eea0181656d5420e
Du Moulinet D'Hardemare Hugues <hugues.du-moulinet-dhardemare@student-cs.fr>
2025-02-26 16:29:11 +0100

    add of tools functions

---
+++ loader_data.py
@@ -0,0 +1,5 @@
+
+
+chantiers_df = pd.read_excel('/Users/thomasdesforges/Desktop/CS/2A/ST7/Projet ST7/instance_WPY_realiste_jalon1.xlsx', sheet_name='Chantiers')
+machines_df = pd.read_excel('/Users/thomasdesforges/Desktop/CS/2A/ST7/Projet ST7/instance_WPY_realiste_jalon1.xlsx', sheet_name='Machines')
+sillons_arrivee_df = pd.read_excel('/Users/thomasdesforges/Desktop/CS/2A/ST7/Projet ST7/instance_WPY_realiste_jalon1.xlsx', sheet_name='Sillons arrivee')
