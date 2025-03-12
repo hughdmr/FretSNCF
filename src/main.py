013e941daeed4349255f211ca64c6b860c6a93a6
Du Moulinet D'Hardemare Hugues <hugues.du-moulinet-dhardemare@student-cs.fr>
2025-03-12 11:16:10 +0100

    removing of sys module

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from model_jalon1 import ModelJalon1
-import sys
 
 if __name__ == '__main__':
     try:
@@ -7,4 +6,3 @@ if __name__ == '__main__':
         print(f"Model created successfully for instance.")
     except Exception as e:
         print(f"An error occurred while creating the model: {e}")
-        sys.exit(1)
