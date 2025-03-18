641884ae303b426226036b1d5502065c9f9bfd25
Angus Chisholm <ac2518@cam.ac.uk>
2025-03-18 12:43:14 +0100

    init commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from model_jalon1 import ModelJalon1
 from model_jalon2 import ModelJalon2
+from model_jalon3 import ModelJalon3
 
 if __name__ == '__main__':
     try:
-        ModelJalon2().run_optimization() # Choose the model you want to run
+        ModelJalon3().run_optimization() # Choose the model you want to run
         print(f"Model created successfully for instance.")
     except Exception as e:
         print(f"An error occurred while creating the model: {e}")
